7d3e399531e8723e1188b803889600e5aee2b143
rzli2106 <rzli2106@gmail.com>
2026-01-01 21:56:46 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/richard-s-research-hub/",
   server: {
     host: "::",
     port: 8080,
